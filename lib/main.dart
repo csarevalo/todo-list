1d3f1647c550c8d2aca78fe1c574fe0634ca1af0
Cristian Arevalo <csarevalo1@gmail.com>
2024-08-28 23:13:08 -0700

    added task_preferences_controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:snazzy_todo_list/src/providers/task_preferences_controller.dart';
 
 import 'src/providers/task_provider.dart';
 import 'src/providers/settings_controller.dart';
@@ -6,12 +8,24 @@ import 'src/utils/settings_service.dart';
 import 'src/todo_app.dart';
 
 void main() async {
-  final settingsController = SettingsController(SettingsService());
-  await settingsController.loadSettings();
   final taskProvider = TaskProvider(TaskProviderService());
   await taskProvider.init();
-  runApp(TodoApp(
-    settingsController: settingsController,
-    taskProvider: taskProvider,
-  ));
+
+  final taskPreferencesController = TaskPreferencesController();
+  await taskPreferencesController.loadPreferences();
+
+  final settingsController = SettingsController(SettingsService());
+  await settingsController.loadSettings();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => taskProvider),
+        ChangeNotifierProvider(create: (context) => taskPreferencesController),
+      ],
+      child: TodoApp(
+        settingsController: settingsController,
+      ),
+    ),
+  );
 }
--- lib/src/todo_app.dart
+++ lib/src/todo_app.dart
@@ -1,49 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'providers/task_provider.dart';
 import 'providers/settings_controller.dart';
 import 'screens/home_screen.dart';
 import 'screens/settings_view.dart';
 
 class TodoApp extends StatelessWidget {
   final SettingsController settingsController;
-  final TaskProvider taskProvider;
   const TodoApp({
     super.key,
     required this.settingsController,
-    required this.taskProvider,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (BuildContext context) => taskProvider,
-      child: ListenableBuilder(
-        listenable: settingsController,
-        builder: (BuildContext context, Widget? child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: "Snazzy To-Do List",
-            // theme: ThemeData(useMaterial3: true),
-            // darkTheme: ThemeData.dark(useMaterial3: true),
-            theme: settingsController.appTheme.light(),
-            highContrastTheme: settingsController.appTheme
-                .lightContrast(settingsController.contrast),
-            darkTheme: settingsController.appTheme.dark(),
-            highContrastDarkTheme: settingsController.appTheme
-                .darkContrast(settingsController.contrast),
-            themeMode: settingsController.themeMode,
-            restorationScopeId: 'todoApp',
-            initialRoute: '/',
-            routes: {
-              '/': (ctx) => HomeScreen(settingsController: settingsController),
-              '/settings': (ctx) =>
-                  SettingsView(controller: settingsController),
-            },
-          );
-        },
-      ),
+    return ListenableBuilder(
+      listenable: settingsController,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "Snazzy To-Do List",
+          // theme: ThemeData(useMaterial3: true),
+          // darkTheme: ThemeData.dark(useMaterial3: true),
+          theme: settingsController.appTheme.light(),
+          highContrastTheme: settingsController.appTheme
+              .lightContrast(settingsController.contrast),
+          darkTheme: settingsController.appTheme.dark(),
+          highContrastDarkTheme: settingsController.appTheme
+              .darkContrast(settingsController.contrast),
+          themeMode: settingsController.themeMode,
+          restorationScopeId: 'todoApp',
+          initialRoute: '/',
+          routes: {
+            '/': (ctx) => HomeScreen(settingsController: settingsController),
+            '/settings': (ctx) => SettingsView(controller: settingsController),
+          },
+        );
+      },
     );
   }
 }
