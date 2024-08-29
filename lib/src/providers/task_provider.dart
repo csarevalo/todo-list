758eca18dcbace9cb76fcf6dc1c16937ea2278fd
Cristian Arevalo <csarevalo1@gmail.com>
2024-08-28 22:33:20 -0700

    combine task_provider with its utility services

--- lib/src/providers/task_provider.dart
+++ lib/src/providers/task_provider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../utils/task_provider_service.dart';
 import '../models/task.dart';
 
 class TaskProvider with ChangeNotifier {
@@ -90,4 +89,65 @@ class TaskProvider with ChangeNotifier {
     _todoList[index].priority = newPriority;
     notifyListeners();
   }
+}
+
+class TaskProviderService {
+  Future<List<Task>> loadTasks() async {
+    DateTime rn = DateTime.now();
+    return [
+      Task(
+        id: -1,
+        title: "Task 1",
+        isDone: false,
+        priority: 3,
+        dateCreated: rn,
+        dateDue: rn,
+        hasDueByTime: false,
+        dateModified: rn,
+      ),
+      Task(
+        id: -2,
+        title: "Task 2",
+        isDone: false,
+        priority: 2,
+        dateCreated: rn,
+        dateDue: rn,
+        hasDueByTime: false,
+        dateModified: rn,
+      ),
+      Task(
+        id: -3,
+        title: "Task 3",
+        isDone: false,
+        priority: 1,
+        dateCreated: rn,
+        dateDue: rn,
+        hasDueByTime: false,
+        dateModified: rn,
+      ),
+      Task(
+        id: -4,
+        title: "Task 4",
+        isDone: false,
+        priority: 3,
+        dateCreated: rn,
+        dateDue: rn,
+        hasDueByTime: false,
+        dateModified: rn,
+      ),
+      Task(
+        id: -5,
+        title: "Task 5",
+        isDone: false,
+        priority: 0,
+        dateCreated: rn,
+        dateDue: rn,
+        hasDueByTime: false,
+        dateModified: rn,
+      ),
+    ];
+  }
+
+  /// Persist all changes made to Todo List
+  Future<void> saveTodoList(int taskId, int newPriority) async {}
 }
