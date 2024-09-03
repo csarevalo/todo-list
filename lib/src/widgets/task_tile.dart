33cde8a6848ef30ff7de9dac7fc1ecc5236db605
Cristian Arevalo <csarevalo1@gmail.com>
2024-09-02 23:50:27 -0700

    currently having issues with stateful widget

--- lib/src/widgets/task_tile.dart
+++ lib/src/widgets/task_tile.dart
@@ -55,7 +55,7 @@ class _CloseSlidableOnTapState extends State<_CloseSlidableOnTap>
   @override
   void dispose() {
     super.dispose();
-    // controller.dispose();
+    controller.dispose();
   }
 
   @override
